3c7cc56d10d27d23356a8699185def7ff9420cc0
Justin Hedrick <Justinchedrick@gmail.com>
2022-04-18 11:17:38 -0700

    part 1 of lab

---
+++ controllers/flights.js
@@ -0,0 +1,25 @@
+const Flight = require('../models/flight');
+
+module.exports = {
+    index,
+    new: newFlight,
+    create
+};
+
+function create(req, res) {
+    const flight = new Flight(req.body);
+    flight.save(function(err){
+        if (err) return res.render('flights/new');
+        res.redirect('/flights/index');
+    });
+}
+
+function newFlight(req, res) {
+    res.render('flights/new');
+}
+
+function index(req, res) {
+    Flight.find({}, function(err, flights){
+        res.render('flights/index', { flights });
+    });
+}
